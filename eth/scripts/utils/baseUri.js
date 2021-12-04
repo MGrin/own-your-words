77be4b79d56410e7adaeb851f836e9c02ba9f37c
MGrin <mr6r1n@gmail.com>
2021-12-04 23:24:23 +0300

    some changes

--- eth/scripts/utils/baseUri.js
+++ eth/scripts/utils/baseUri.js
@@ -4,7 +4,7 @@ const getBaseUri = (network) => {
       return "http://localhost:4001/";
     }
     case "rinkeby": {
-      throw new Error("BaseURI is not yet defined for rinkeby!");
+      return "https://bbadsh7q3pqrt2iainf0.containers.yandexcloud.net/";
     }
     default: {
       throw new Error(`BaseURI is not yet defined for ${network}!`);
